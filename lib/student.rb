8b438aee90dbe222f4540da7ded05696473c27aa
Mohammad Daylaki <github email address>
2017-03-05 23:24:41 +0300

    in-progressing

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,9 @@
 require_relative "../config/environment.rb"
 
 class Student
-
+  attr_accessor :name
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
- 
+  #  with DB[:conn]
+
+
 end
